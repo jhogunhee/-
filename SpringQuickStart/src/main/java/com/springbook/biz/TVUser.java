9a3fbcb283366fcef6726cbd0ba39117bfed8ffb
조건희 <rjsgml1677@naver.com>
2021-03-01 14:12:50 +0900

    디자인 패턴 이용하기 2021-03-01

--- SpringQuickStart/src/main/java/com/springbook/biz/TVUser.java
+++ SpringQuickStart/src/main/java/com/springbook/biz/TVUser.java
@@ -4,8 +4,9 @@ public class TVUser {
 	public static void main(String[] args) {
 		// 묵시적 형 변환을 이용하여 객체를 참조하면 
 		// 참조하는 객체만 변경하면 되므로 객체를 쉽게 변경이 가능
-//		TV tv = new SamsungTV();
-		TV tv = new LgTV();
+		BeanFactory factory = new BeanFactory();
+		TV tv = (TV) factory.getBean(args[0]);
+//		TV tv = (TV) factory.getBean(System.getProperty("SamsungTV"));
 		tv.powerOn();
 		tv.powerOff();
 		tv.volumeUp();
